55d85f80f64c0e9efa448bf0c48df63c784b2ec7
Mauricio Merlin <MauricioMerlin@mauricio-MacBook.local>
2019-05-08 16:20:23 -0300

    add: uploadController

--- app/controller/uploadController.js
+++ app/controller/uploadController.js
@@ -1,34 +1,13 @@
-const model = require("../model").user;
+const model = require("../model").data;
 var exports = module.exports = {};
 
 exports.create = function (req, res) {
-   model.create({
-      name: req.body.name,
+   
+
+
+	:wmodel.create({
+      file: req.body.file,
       email: req.body.email,
       password: req.body.password,
    }).then(user => res.json(user));
 };
-
-exports.read = function (req, res) {
-    model.findAll()
-        .then(user => res.json(user))
-        .error(err => res.json(err));
-};
-
-exports.login = function (req ,res) {
-    model.find({
-  	where: {
-        email: req.body.email,
-   	    password: req.body.password
-    }
-    })
-   	.then(user => {
-        console.log('user ' + user);
-        if (user){
-            res.json(user);
-        } else {
-            res.status(403).render();
-        }
-	})
-    .error(err => res.json(err));
-}
